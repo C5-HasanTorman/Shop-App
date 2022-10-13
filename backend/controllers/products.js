d17240a6324829c6c6633ba7d72562888e662c41
HasanTorman <torman.hassan@gmail.com>
2022-10-13 16:19:02 +0300

    create products backend functionality (get Product By Id )

--- backend/controllers/products.js
+++ backend/controllers/products.js
@@ -27,21 +27,51 @@ const addNewProduct = (req, res) => {
 // get all Products
 
 const getAllProduct = (req, res) => {
-    const query = `SELECT * FROM Products WHERE is_deleted=0;`;
-    connection.query(query, (err, result) => {
-      if (err) {
-        return res.status(500).json({
-          success: false,
-          massage: "server error",
-          err: err,
-        });
-      }
-      return res.status(200).json({
-        success: true,
-        massage: "All the items",
-        result: result,
+  const query = `SELECT * FROM Products WHERE is_deleted=0;`;
+  connection.query(query, (err, result) => {
+    if (err) {
+      return res.status(500).json({
+        success: false,
+        massage: "server error",
+        err: err,
       });
+    }
+    return res.status(200).json({
+      success: true,
+      massage: "All the items",
+      result: result,
     });
-  };
+  });
+};
 
-module.exports = { addNewProduct,getAllProduct };
+//get product by id
+
+const getProductById = (req, res) => {
+  const products_id = req.params.id;
+
+  const query = `SELECT * FROM products WHERE id=? AND is_deleted=0;`;
+  const data = [products_id];
+
+  connection.query(query, data, (err, result) => {
+    if (err) {
+      return res.status(500).json({
+        success: false,
+        massage: "Server Error",
+        err,
+      });
+    }
+    if (!result.length) {
+      return res.status(404).json({
+        success: false,
+        massage: "The product is Not Found",
+      });
+    }
+    return res.status(200).json({
+      success: true,
+      massage: `product NO. ${products_id}`,
+      result,
+    });
+  });
+};
+
+module.exports = { addNewProduct, getAllProduct, getProductById };
--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,10 +1,15 @@
 const express = require("express");
-const { addNewProduct, getAllProduct } = require("../controllers/products");
+const {
+  addNewProduct,
+  getAllProduct,
+  getProductById,
+} = require("../controllers/products");
 const auth = require("../middleware/authentication");
 
 const productsRouter = express.Router();
 
 productsRouter.post("/", auth, addNewProduct);
 productsRouter.get("/", getAllProduct);
+productsRouter.get("/:id", getProductById);
 
 module.exports = productsRouter;
