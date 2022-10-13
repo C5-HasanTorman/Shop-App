beda761505c840606fe27fcfef1ae8706373c53f
HasanTorman <torman.hassan@gmail.com>
2022-10-13 13:46:05 +0300

    backend strtup files

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const PORT = 5000;
+
+//built-in middleware
+app.use(cors());
+app.use(express.json());
+
+// Handles any other endpoints [unassigned - endpoints]
+app.use("*", (req, res) => res.status(404).json("NO content at this path"));
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
+});
