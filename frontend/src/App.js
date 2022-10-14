b3a8e9f0dfe4bfb498c211cb720751f8b0b15c69
HasanTorman <torman.hassan@gmail.com>
2022-10-14 07:15:15 +0300

    create register component frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Register from './Components/Register';
 function App() {
   return (
     <div className="App">
+      <Register/>
 
     </div>
   );
