1f7d372d98e74f35317359ec2dcc2edfe0a968e7
HasanTorman <torman.hassan@gmail.com>
2022-10-14 07:24:25 +0300

    create redux files +configur store

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Register from './Components/Register';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Route, Routes } from "react-router-dom";
+import Register from "./Components/Register";
 function App() {
   return (
     <div className="App">
-      <Register/>
-
+      <Routes>
+        <Route path="/register" element={<Register />} />
+      </Routes>
     </div>
   );
 }
